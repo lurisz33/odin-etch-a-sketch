06cffaecb11d9a99370407747922d0d874e4a8b7
lukas paul <lukas@172.31.172.200>
2024-05-21 15:08:21 +0200

    added EventListeners for every div with random rgb background
    every div changes its background color from the default to a random one when the mouse hovers over it, works multiple times

--- myscripts.js
+++ myscripts.js
@@ -19,13 +19,24 @@ function setSquareSize(size) {
     squareContainer.style.height = "auto%";
 
 
-    const squareSize = Math.floor(100 / size); // Calculate size percentage for flex basis
-
     for (let i = 0; i < size * size; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("square");
         newDiv.style.height = `${100 / size}%`;
         newDiv.style.flex = `0 0 ${100 / size}%`;
+        newDiv.addEventListener("mouseover", function () {
+            newDiv.style.backgroundColor = getRandomRGBColor();
+        })
         squareContainer.appendChild(newDiv);
     }
+}
+
+function getRandomRGBColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return `rgb(${r}, ${g}, ${b})`;}
+
+function randomInteger(max) {
+    return Math.floor(Math.random() * (max + 1));
 }
